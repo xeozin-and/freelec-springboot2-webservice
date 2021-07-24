7dc404abe2c9d8050154ae557842dcea2cbad32c
seo jin <comcxx11@gmail.com>
2021-07-24 17:38:42 +0900

    JDBC 관련 코드 제거

--- src/main/java/kr/co/duolabs/springboot/web/HelloController.java
+++ src/main/java/kr/co/duolabs/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package kr.co.duolabs.springboot.web;
 
+import kr.co.duolabs.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/kr/co/duolabs/springboot/HelloControllerTest.java
+++ src/test/java/kr/co/duolabs/springboot/HelloControllerTest.java
@@ -1,5 +1,6 @@
 package kr.co.duolabs.springboot;
 import kr.co.duolabs.springboot.web.HelloController;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,6 +11,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
@@ -17,9 +19,20 @@ public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
-        mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk()) // st1atus 200 확인
+                .andExpect(content().string(hello)); // body 확인
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws  Exception {
+        String name = "hello";
+        int amount = 1000;
+
+
     }
 }
